724da7c6054d16b7f417d925add1d51e18d6d1c3
Hoang Huu Cuong <hoanghuuhanh321@gmail.com>
2023-09-20 23:20:37 +0700

    Up Lan2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var url = "http://localhost:3000/comments"
 
 var arrayTitle = new Array()
-
+var Url1;
 async function GetUrl() {
     try {
         const reporise = await fetch(url);
@@ -10,12 +10,20 @@ async function GetUrl() {
         }
         const Value = await reporise.json();
         Value.map(TiTle => {
-            arrayTitle.push(`
-               
-                <h1>${TiTle.body}</h1>
-                <p>${TiTle.postId}</p>
-                <button onclick = Delete(${TiTle.id})>DeleTe</button>
-            `)
+            if (url + '/ ' + TiTle.id == Url1) {
+                console.log("A")
+            } else {
+
+                arrayTitle.push(`
+                    <div id = id_${TiTle.id}>
+                        <h1 id= "TextName">${TiTle.TiTleName}</h1>
+                        <p id ="TextContent">${TiTle.Titlebody}</p>
+                        <button onclick = Delete(${TiTle.id})>DeleTe</button>
+                        <button onclick = ListenApi(${TiTle.id})>Edit</button>
+
+                    </div>
+               `)
+            }
         })
         tExtContent(arrayTitle)
     }
@@ -26,37 +34,82 @@ async function GetUrl() {
 GetUrl()
 
 async function CreateApi() {
-    const Titlebody = await document.getElementById("MyName").value
-    const TiTlepostId = await document.getElementById("Text").value
+    const TiTleName = await document.getElementById("MyName").value
+    const Titlebody = await document.getElementById("Text").value
     const CreateName = await fetch('http://localhost:3000/comments', {
         method: 'POST',
         body: JSON.stringify({
-            body: Titlebody,
-            postId: TiTlepostId,
+            TiTleName: TiTleName,
+            Titlebody: Titlebody,
 
         }),
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
         }
     })
-    await CreateName.json();
-    GetAPi();
 }
 
 async function Delete(id) {
-
+    console.log(url + '/' + id)
     const putMethod = {
-        method: 'DELETE', // Method itself
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8' // Indicates the content 
-        },
-        // body: JSON.stringify(url) // We send data in JSON format
+        method: 'DELETE',
     }
     fetch(url + '/' + id, putMethod)
         .then(reposie => {
             reposie.json()
         })
-        .then(GetUrl())
+
+
+}
+
+async function ListenApi(id) {
+    var divMap = document.getElementById(`id_${id}`)
+    var TextName = divMap.getElementsByTagName("h1")[0].textContent;
+    var TextBody = divMap.getElementsByTagName("p")[0].textContent;
+    console.log(typeof TextName, typeof TextBody)
+    divMap.innerHTML = `
+            <h1 id= "TextName">${TextName}</h1>
+            <p id ="TextContent">${TextBody}</p>
+            <select  id ="Opption_${id}">
+                <option>TextName</option>
+                <option>TextBody</option>
+            </select>
+            <input id ="Text_${id}" type="text">
+            <button onclick="LisTenControl(${id})">Save</button>
+         `
+}
+async function LisTenControl(id) {
+    var ValueInput = document.getElementById(`Opption_${id}`).value // Get Opption Value
+    var TextInput = document.getElementById(`Text_${id}`).value // Get Value Input
+    var divMap = document.getElementById(`id_${id}`) // Get Div id
+    if (ValueInput == "TextName") {
+        var TextName = divMap.getElementsByTagName("h1")[0].textContent; // Get TextName
+        var TextBody = divMap.getElementsByTagName("p")[0].textContent; // GetTextBody
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                TiTleName: TextInput,
+                Titlebody: TextBody,
+            }),
+        };
+        fetch(url + '/' + id, requestOptions)
+
+    } else if (ValueInput == "TextBody") {
+        var TextName = divMap.getElementsByTagName("h1")[0].textContent; // Get TextName
+        console.log(TextName)
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                TiTleName: TextName,
+                Titlebody: TextInput,
+            }),
+
+        };
+        console.log(TextName, TextInput)
+        fetch(url + '/' + id, requestOptions)
+    }
 
 }
 
@@ -65,4 +118,3 @@ function tExtContent(TiTle) {
     Text.innerHTML = TiTle.join("")
 
 }
-
